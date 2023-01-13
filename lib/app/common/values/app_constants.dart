dbc3dbf760fc53e71890475e41bbac66c4e92d5b
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2023-01-13 11:53:25 +0700

    update: api host url

--- lib/app/common/values/app_constants.dart
+++ lib/app/common/values/app_constants.dart
@@ -1,7 +1,7 @@
 abstract class AppConstants {
   static String token = "token";
-  static String baseAPIURL = "http://192.168.0.103:3000/api/v1";
-  static String baseURL = "http://192.168.0.103:3000/";
+  static String baseAPIURL = "http://202.83.122.66:60601/api/v1";
+  static String baseURL = "http://202.83.122.66:60601/";
 
   static String rolePsikolog = "psikolog";
   static String roleUser = "user";
